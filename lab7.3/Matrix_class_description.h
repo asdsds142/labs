2c62f518b1596101baf03ad51b3231617e36b9da
asdsds142 <asdsds1421@gmail.com>
2022-11-16 00:36:56 +0300

    матрица, еще надо дооптимизировать операторы сделать без унарного минуса минусы

---
+++ lab7.3/Matrix_class_description.h
@@ -0,0 +1,78 @@
+//Matrix_class_description.h
+/*Сначала реализовывал на векторах но вектора как то по тупому работают,
+  двумерный вектор плохо копируется вызывая сегфолты да и много других проблем
+  поетому я все же реализую на классическом двойном указателе динамическом массиве
+  а потом как изучу умные указатели возможно чуть докручу*/
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include "../tools/tools.h"
+
+
+
+using namespace std;
+
+//базовый класс для матрицы, потом подкручу наследование чтобы наследовать кастомние
+template<class T>
+class Matrix
+{
+private:
+    static constexpr uint64_t DEFAULT_SIZE = 50;
+    static uint64_t matrix_id_;
+
+	uint64_t string_number_ = DEFAULT_SIZE;
+    uint64_t column_number_ = DEFAULT_SIZE;
+    T** body_ = nullptr;
+
+
+public:
+
+	explicit Matrix(uint64_t = DEFAULT_SIZE, uint64_t = DEFAULT_SIZE, T = 0);//													
+    explicit Matrix(istream&); //
+
+    Matrix(const Matrix<T>&); //
+    ~Matrix();
+    Matrix<T>& operator=(const Matrix<T>&);
+
+	void to_file(ofstream&); //
+	void show() const; //
+
+    uint64_t get_i() const {return this->string_number_;}
+    uint64_t get_j() const {return this->column_number_;}
+    T get_value(uint64_t, uint64_t) const; //
+
+    T string_sum(uint64_t);
+    T column_sum(uint64_t);
+    T min(); //
+    T max(); //
+    vector<T> get_min_vector();
+
+
+    Matrix<T> operator-() const; //
+    Matrix<T>& operator*=(const Matrix<T>&); 
+	Matrix<T> operator*(const Matrix<T>&) const; //
+    Matrix<T>& operator+=(const Matrix<T>&); //
+    Matrix<T> operator+(const Matrix<T>&) const; //
+    Matrix<T>& operator-=(const Matrix<T>&); //
+    Matrix<T> operator-(const Matrix<T>&) const; //
+
+    Matrix<T> operator^(uint64_t) const; //
+
+    bool operator==(const Matrix<T>&) const; 
+
+    Matrix<T>& operator*=(T);
+    Matrix<T> operator*(T const);
+    Matrix<T>& operator/=(T);
+    Matrix<T> operator/(T) const;
+
+
+    template<class P>
+    friend ostream& operator<<(ostream&, const Matrix<P>&);
+    template<class P>
+    friend istream& operator>>(istream&, Matrix<P>&);
+	
+};
---
+++ lab7.3/matrix_tester.cpp
@@ -0,0 +1,35 @@
+#include "Matrix.h"
+
+int main()
+{
+    auto input_stream = ifstream("matrix1");
+    auto m_first = Matrix<int64_t>(input_stream);
+    cout << m_first;
+    try
+    {
+        auto m_second = m_first * m_first;
+        cout << m_second;
+        m_second += m_second;
+        m_second = m_first;
+        cout << m_second << m_first;
+
+        auto m_third = Matrix<int64_t>(7, 7);
+        cout << m_third;
+        m_third = m_second;
+        cout << m_third;
+
+        auto second_input_stream = ifstream("matrix2");
+        second_input_stream >> m_third;
+        cout << m_third;
+        m_third *= 26;
+        cout << m_third;
+        m_third = -m_third;
+        cout << m_third;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "что-то сломалосб" << endl;
+        std::cerr << e.what() << '\n';
+    }    
+}
+

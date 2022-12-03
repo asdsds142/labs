be2762c5333f8f5c262e6130fc1fabb16fa78fc9
asdsds142 <asdsds1421@gmail.com>
2022-12-04 02:00:42 +0300

    vulkan GE after lesson4 tmp save

--- Vulkan_test/lve_window.cpp
+++ Vulkan_test/lve_window.cpp
@@ -1,5 +1,8 @@
 #include "lve_window.hpp"
 
+//std
+#include <stdexcept>
+
 namespace lve {
 
 LveWindow::LveWindow(int32_t w, int32_t h, std::string name): 
@@ -23,6 +26,15 @@ void LveWindow::initWindow()
 
     window = glfwCreateWindow(width, height, window_name.c_str(), nullptr, nullptr);
 }
+
+    void LveWindow::createWindowSurface(VkInstance instance, VkSurfaceKHR* surface)
+    {
+        if (glfwCreateWindowSurface(instance, window, nullptr, surface) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create window surface");
+        }
+    }
+    
 
 
 } // namespace lve
